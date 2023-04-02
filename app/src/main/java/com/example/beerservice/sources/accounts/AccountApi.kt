b0ce34af99c2c51fb4f6b0a9837aac659b5f4790
Eugene <jsolomko@bk.ru>
2023-04-02 18:48:20 +0300

    signIn

--- app/src/main/java/com/example/beerservice/sources/accounts/AccountApi.kt
+++ app/src/main/java/com/example/beerservice/sources/accounts/AccountApi.kt
@@ -1,5 +1,6 @@
 package com.example.beerservice.sources.accounts
 
+import com.example.beerservice.sources.accounts.entites.SignInRequestEntity
 import com.example.beerservice.sources.accounts.entites.SignUpRequestEntity
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -8,4 +9,7 @@ interface AccountApi {
 
     @POST("user/create")
     fun signUp(@Body body: SignUpRequestEntity)
+
+    @POST("/user/auth")
+    fun signIn(@Body signInRequestEntity: SignInRequestEntity)
 }
