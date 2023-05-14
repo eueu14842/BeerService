5198a412c032d3f69794d588a67335ade2437bb5
Eugene <jsolomko@bk.ru>
2023-05-14 15:11:54 +0300

    -обновил граф точек продаж в табах

--- app/src/main/java/com/example/beerservice/app/screens/main/tabs/home/places/tabs/PlaceContainerFragment.kt
+++ app/src/main/java/com/example/beerservice/app/screens/main/tabs/home/places/tabs/PlaceContainerFragment.kt
@@ -6,24 +6,22 @@ import android.os.Bundle
 import android.view.View
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
 import androidx.viewpager2.widget.ViewPager2
 import com.example.beerservice.R
 import com.example.beerservice.app.screens.main.tabs.home.places.adapters.PlaceCollectionAdapter
-import com.example.beerservice.databinding.FragmentPlacesBinding
+import com.example.beerservice.databinding.FragmentPlaceContainerBinding
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
-class PlaceContainerFragment : Fragment(R.layout.fragment_places) {
-    lateinit var binding: FragmentPlacesBinding
+class PlaceContainerFragment : Fragment(R.layout.fragment_place_container) {
+    lateinit var binding: FragmentPlaceContainerBinding
     private lateinit var placesCollectionAdapter: PlaceCollectionAdapter
     private lateinit var viewPager: ViewPager2
     private lateinit var tabLayout: TabLayout
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding = FragmentPlacesBinding.bind(view)
+        binding = FragmentPlaceContainerBinding.bind(view)
         setupViews()
         placesCollectionAdapter = PlaceCollectionAdapter(this)
         viewPager.adapter = placesCollectionAdapter
